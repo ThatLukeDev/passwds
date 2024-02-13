d5db14ef476729fc45b3c0be0305290741240327
ThatLukeDev <luke@l2j.co.uk>
2024-02-13 10:32:56 +0000

    gui

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <gtk/gtk.h>
 
-static void print_hello(GtkWidget *widget, gpointer data) {
-	g_print("Hello World\n");
+static void generate(GtkWidget *widget, gpointer data) {
+	g_print("generate\n");
+}
+
+static void check(GtkWidget *widget, gpointer data) {
+	g_print("check\n");
+}
+
+static void save(GtkWidget *widget, gpointer data) {
+	g_print("save\n");
+}
+
+static void load(GtkWidget *widget, gpointer data) {
+	g_print("load\n");
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
@@ -20,18 +32,44 @@ static void activate(GtkApplication *app, gpointer user_data) {
 	gtk_widget_set_margin_end(grid, 8);
 	gtk_window_set_child(GTK_WINDOW(window), grid);
 
-	GtkWidget* button = gtk_button_new_with_label("Hello World");
-	g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-	gtk_grid_attach(GTK_GRID(grid), button, 0, 0, 1, 1);
+	GtkWidget* nameEntry = gtk_entry_new();
+	GtkWidget* nameLabel = gtk_label_new_with_mnemonic("Name: ");
+	gtk_label_set_mnemonic_widget(GTK_LABEL(nameLabel), nameEntry);
+	gtk_label_set_xalign(GTK_LABEL(nameLabel), 1.0f);
+	gtk_grid_attach(GTK_GRID(grid), nameLabel, 0, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), nameEntry, 1, 0, 3, 1);
+
+	GtkWidget* passwdEntry = gtk_entry_new();
+	GtkWidget* passwdLabel = gtk_label_new_with_mnemonic("Password: ");
+	gtk_label_set_mnemonic_widget(GTK_LABEL(passwdLabel), passwdEntry);
+	gtk_label_set_xalign(GTK_LABEL(passwdLabel), 1.0f);
+	gtk_grid_attach(GTK_GRID(grid), passwdLabel, 0, 1, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), passwdEntry, 1, 1, 3, 1);
+
+	GtkWidget* generateButton = gtk_button_new_with_label("Generate");
+	g_signal_connect(generateButton, "clicked", G_CALLBACK(generate), NULL);
+	gtk_grid_attach(GTK_GRID(grid), generateButton, 0, 2, 2, 1);
+
+	GtkWidget* checkButton = gtk_button_new_with_label("Check");
+	g_signal_connect(checkButton, "clicked", G_CALLBACK(check), NULL);
+	gtk_grid_attach(GTK_GRID(grid), checkButton, 2, 2, 2, 1);
+
+	GtkWidget* saveButton = gtk_button_new_with_label("Save");
+	g_signal_connect(saveButton, "clicked", G_CALLBACK(save), NULL);
+	gtk_grid_attach(GTK_GRID(grid), saveButton, 0, 3, 2, 1);
+
+	GtkWidget* loadButton = gtk_button_new_with_label("Load");
+	g_signal_connect(loadButton, "clicked", G_CALLBACK(load), NULL);
+	gtk_grid_attach(GTK_GRID(grid), loadButton, 2, 3, 2, 1);
 
 	gtk_window_present(GTK_WINDOW(window));
 }
 
 int main(int argc, char** argv) {
 	#if GLIB_CHECK_VERSION(2, 74, 0)
-		GtkApplication* app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+		GtkApplication* app = gtk_application_new("org.tld.passwd", G_APPLICATION_DEFAULT_FLAGS);
 	#else
-		GtkApplication* app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+		GtkApplication* app = gtk_application_new("org.tld.passwd", G_APPLICATION_FLAGS_NONE);
 	#endif
 	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 	int status = g_application_run(G_APPLICATION(app), argc, argv);
